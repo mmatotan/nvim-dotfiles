503a470ecc0917892e096abd5200054b823ec8f2
Marko Matotan <marko.matotan4@gmail.com>
2025-05-03 18:30:08 +0200

    Telescope fuzzy file find

--- lua/marko/options.lua
+++ lua/marko/options.lua
@@ -1,5 +1,4 @@
 o = vim.opt
-
 local options = {
   backup = false,                          -- creates a backup file
   clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
@@ -18,17 +17,17 @@ local options = {
   splitbelow = true,                       -- force all horizontal splits to go below current window
   splitright = true,                       -- force all vertical splits to go to the right of current window
   swapfile = false,                        -- creates a swapfile
-  -- termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 1000,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  -- termguicolors = true,                 -- set term gui colors (most terminals support this)
+  timeoutlen = 1000,                       -- time to wait for a mapped sequence to complete (in milliseconds)
   undofile = true,                         -- enable persistent undo
   updatetime = 300,                        -- faster completion (4000ms default)
   writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true,                        -- convert tabs to spaces
+  expandtab = true,                        -- convert tabs to spacesopti
   shiftwidth = 2,                          -- the number of spaces inserted for each indentation
   tabstop = 2,                             -- insert 2 spaces for a tab
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
-  relativenumber = true,                  -- set relative numbered lines
+  relativenumber = true,                   -- set relative numbered lines
   numberwidth = 4,                         -- set number column width to 2 {default 4}
   signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
   wrap = false,                            -- display lines as one long line
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,20 +1,51 @@
 return {
+  -- FZF
+  {
+    'nvim-telescope/telescope-fzf-native.nvim', build = 'make'
+  },
+
+  -- Telescope
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
      dependencies = { 'nvim-lua/plenary.nvim' },
      config = function ()
       local configs = require("telescope")
        
-      configs.setup({
+      configs.setup(
+      {
         defaults = {
+          layout_config = {
+            horizontal = {
+              width = 0.9
+            }
+          },
           mappings = {
-            i = {}
+            i = {
+              ["<C-j>"] = "move_selection_next",
+              ["<C-k>"] = "move_selection_previous",
+              ["<A-k>"] = "preview_scrolling_up",
+              ["<A-j>"] = "preview_scrolling_down"
+            },
+            n = {
+              ["<A-k>"] = "preview_scrolling_up",
+              ["<A-j>"] = "preview_scrolling_down"
+            }
+          }
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,                  
+            override_generic_sorter = true,
+            override_file_sorter = true,   
+            case_mode = "smart_case",      
           }
         }
       })
-
+      require('telescope').load_extension('fzf')
      end
   },
+
+  -- Treesitter
   {
      'nvim-treesitter/nvim-treesitter', build = ':TSUpdate',
       config = function () 
